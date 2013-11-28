69ebc9678d6977173b7ca955a2c3cc97af08a87d
Michael McManus <michaelorionmcmanus@gmail.com>
2013-11-28 12:58:18 -0800

    Site search and other improvements.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,9 @@
 /*global define */
 define([], function () {
     'use strict';
-
-    return '\'Allo \'Allo!';
+    var link = document.querySelector('link[href="main-nav.html"]');
+    var content = link.import;
+    // Grab DOM from warning.html's document.
+    var el = content.querySelector('body');
+    document.querySelector('header[name="main"]').appendChild(el.cloneNode(true));
 });
