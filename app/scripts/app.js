82c964646f16adfee82caaa58416d5dc016679c3
Michael McManus <michaelorionmcmanus@gmail.com>
2013-11-28 23:47:12 -0800

    Simple search :(

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,4 @@
 /*global define */
 define(['jquery'], function ($) {
   'use strict';
-
-  function searchOn() {
-    $('.search-container').addClass('search-active');
-    $('#site-search').focus();
-  };
-  var trigger = $('#search-trigger');
-  trigger.on('click', function() {
-    searchOn();
-    return false;
-  });
-
-  $('#site-search').on('blur', function() {
-    $('.search-container').removeClass('search-active');
-  });
-
-  $('#site-search').on('focus', function() {
-    searchOn();
-  });
 });
